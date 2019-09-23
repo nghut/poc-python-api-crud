08b1f77014abf4a30f63a1975c8275ba9cbc6dbc
nghut <nghut02@gmail.com>
2019-09-23 21:38:24 +0530

    views and urls

--- webapp/serializers.py
+++ webapp/serializers.py
@@ -1,9 +1,9 @@
 from rest_framework import serializers
-from rest_framework import employees
+from .models import employees
 
-class employeeSerializer(serializers.ModelSerializer)
+class employeesSerializer(serializers.ModelSerializer):
 
-class Meta:
-    model= employees
-    fields=('firstname','lastname')
-    fields= '_all_'
+    class Meta:
+        model = employees
+ #      fields=('firstname','lastname')
+        fields = '__all__'
